1e39abb41c164ddf38513d574a7f391384de6cb2
Robyn Noble <robynhurd09@gmail.com>
2021-08-16 14:14:49 -0500

    practicing print statements and if else statements

---
+++ pyworkshop/name.py
@@ -0,0 +1,2 @@
+name = "Robyn"
+print(name)
---
+++ pyworkshop/name_lib.py
@@ -0,0 +1,7 @@
+def upper_case_name(name):
+    return name.upper()
+
+
+name = "Robyn"
+upper_case_name = upper_case_name(name)
+print(f"Upper case name is {upper_case_name}")
